aa0cd5547e7c70fcbd4c91b0eb062991995ff40f
JoseGaviria24 <josedavidinglesamericano@gmail.com>
2021-09-28 22:40:52 -0500

    HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+package Intento1;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String h="the quick brown fox jumps over the lazy dog";
+		for(int i=0;i<h.length();i++) {
+			System.out.print(h.charAt(i)+"  ");}}}
